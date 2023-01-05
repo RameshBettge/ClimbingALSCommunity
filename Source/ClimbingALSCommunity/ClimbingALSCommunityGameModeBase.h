dffcc8f410404360f8c83c1208ef730b8f5b2a85
RamiCreates <ramesh.creates@gmail.com>
2023-01-05 17:02:15 +0100

    ADDED: Empty UE5.0 project with ALS Community plugin.

---
+++ Source/ClimbingALSCommunity/ClimbingALSCommunityGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClimbingALSCommunityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIMBINGALSCOMMUNITY_API AClimbingALSCommunityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
